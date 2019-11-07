a3adee79942295e5a05b5ae278a881f4c2f85bb5
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-07 17:52:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,8 +10,7 @@ end
 
 def sort_array_char_count(array)
      array.sort do |left, right|
-       left.length <=> right.length
-   end
+  end
 
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
