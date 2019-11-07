bf50066ff99136c36559395404b9e2c934ff7b4c
Jeshua Hicks <jhicks7884@outlook.com>
2019-11-07 17:33:30 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,7 +8,7 @@ def sort_array_desc(array)
   end
 end
 
-sort_array_char_count(["dogs", "cats", "horses"])
+sort_array_char_count(["dogs", "cat", "Horses"]) 
 
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
